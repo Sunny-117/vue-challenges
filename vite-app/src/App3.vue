a4dff08a3619938a8c6d259b84e42b8e68822811
付志强 <zhiqiangfu6@gmail.com>
2023-02-08 13:28:43 +0800

    feat: checkbox

---
+++ vite-app/src/App3.vue
@@ -0,0 +1,44 @@
+<template>
+  checked names: {{ showCheckedNames }}
+  <div v-for="item in checkedNames" :key="item.name">
+    <input type="checkbox" v-model="item.isChecked" /> {{ item.name }}
+  </div>
+
+  <!-- <input type="checkbox" v-model="value" /> 广州 -->
+</template>
+
+<script setup lang="ts">
+import { computed, reactive, ref, watch } from "vue";
+
+const checkedNames = reactive([
+  {
+    name: "北京",
+    value: "beijing",
+    isChecked: false,
+  },
+  {
+    name: "上海",
+    value: "shangahi",
+    isChecked: true,
+  },
+  {
+    name: "广州",
+    value: "guangzhou",
+    isChecked: false,
+  },
+]);
+const showCheckedNames = computed(() => {
+  return checkedNames
+    .filter((item) => {
+      return item.isChecked;
+    })
+    .map((item) => {
+      return item.name;
+    });
+});
+watch(showCheckedNames, (newV) => {
+  console.log("[ newV ] >", newV);
+});
+</script>
+
+<style scoped></style>
