eb30ee4f337c7ad3553a38f83ca7b613c2dc828c
付志强 <zhiqiangfu6@gmail.com>
2023-02-08 13:46:17 +0800

    feat: checkall

--- vite-app/src/App3.vue
+++ vite-app/src/App3.vue
@@ -1,16 +1,16 @@
 <template>
-  checked names: {{ showCheckedNames }}
+  <h1>全选</h1>
+  <label for="checkall">全选</label>
+  <input type="checkbox" :checked="isCheckedAll" @change="handleAllChange" />
   <div v-for="item in checkedNames" :key="item.name">
     <input type="checkbox" v-model="item.isChecked" /> {{ item.name }}
   </div>
-
-  <!-- <input type="checkbox" v-model="value" /> 广州 -->
 </template>
 
 <script setup lang="ts">
-import { computed, reactive, ref, watch } from "vue";
+import { computed, reactive } from "vue";
 
-const checkedNames = reactive([
+let checkedNames = reactive([
   {
     name: "北京",
     value: "beijing",
@@ -27,18 +27,32 @@ const checkedNames = reactive([
     isChecked: false,
   },
 ]);
-const showCheckedNames = computed(() => {
-  return checkedNames
-    .filter((item) => {
-      return item.isChecked;
-    })
-    .map((item) => {
-      return item.name;
+let isCheckedAll = computed(() => {
+  return checkedNames.every((item) => item.isChecked);
+});
+const handleAllChange = (e) => {
+  // 已经全选
+  if (isCheckedAll.value) {
+    console.log("[ 已经是全选，应该全部取消 ] >");
+    checkedNames.forEach((item) => {
+      item.isChecked = false;
     });
-});
-watch(showCheckedNames, (newV) => {
-  console.log("[ newV ] >", newV);
-});
+  } else {
+    // 部分没有选 应该全选中
+    // checkedNames = checkedNames.map((item) => {
+    //   return {
+    //     ...item,
+    //     isChecked: true,
+    //   };
+    // });
+    // 上面的方法不可：
+    // 1. 没有响应式的新数组
+    // 2. 没有对原来的响应式数据变化
+    checkedNames.forEach((item) => {
+      item.isChecked = true;
+    });
+  }
+};
 </script>
 
 <style scoped></style>
